6aa48cfc20d82275e9043b1f16e90d1a7e678588
Lains <4886639+lainsce@users.noreply.github.com>
2022-06-29 12:34:04 -0300

    hopefully this works?

--- panels/background/cc-background-panel.c
+++ panels/background/cc-background-panel.c
@@ -54,14 +54,14 @@
 #define INTERFACE_SHELL_THEME_KEY "name"
 
 // Constants for each accent color
-struct _GVariant * redarr[] = {0.8588, 0.1568, 0.3764};
-struct _GVariant * orangearr[] = {0.9686, 0.5058, 0.168};
-struct _GVariant * yellowarr[] = {0.8784, 0.6313, 0.0039};
-struct _GVariant * greenarr[] = {0.2862, 0.8156, 0.3686};
-struct _GVariant * mintarr[] = {0.3372, 0.7490, 0.6509};
-struct _GVariant * bluearr[] = {0.1490, 0.5568, 0.9764};
-struct _GVariant * purplearr[] = {0.5490, 0.3372, 0.7490};
-struct _GVariant * pinkarr[] = {0.7490, 0.3372, 0.6588};
+GVariant * redarr = g_variant_new ("ddd", {0.8588, 0.1568, 0.3764});
+GVariant * orangearr = g_variant_new ("ddd", {0.9686, 0.5058, 0.168});
+GVariant * yellowarr = g_variant_new ("ddd", {0.8784, 0.6313, 0.0039});
+GVariant * greenarr = g_variant_new ("ddd", {0.2862, 0.8156, 0.3686});
+GVariant * mintarr = g_variant_new ("ddd", {0.3372, 0.7490, 0.6509});
+GVariant * bluearr = g_variant_new ("ddd", {0.1490, 0.5568, 0.9764});
+GVariant * purplearr = g_variant_new ("ddd", {0.5490, 0.3372, 0.7490});
+GVariant * pinkarr = g_variant_new ("ddd", {0.7490, 0.3372, 0.6588});
 
 struct _CcBackgroundPanel
 {
